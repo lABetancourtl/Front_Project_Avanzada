add9e626c4ebe788472fc8976a997a6e62c7ac3c
lABetancourtl <anderson.betancourta@uqvirtual.edu.co>
2025-05-04 16:27:31 -0500

    Inicio de formularios segun la guia 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component }    from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector    : 'app-root',
+  imports     : [RouterOutlet, RouterModule],
+  templateUrl : './app.component.html',
+  styleUrl    : './app.component.css'
 })
 export class AppComponent {
-  title = 'front';
+  title       = 'front';
+  footer      = 'Universidad del Quindio 2025-1';
 }
