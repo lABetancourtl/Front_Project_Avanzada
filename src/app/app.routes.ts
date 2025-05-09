b3632c740df998bb446ea77f76ce566dc75f3246
lABetancourtl <anderson.betancourta@uqvirtual.edu.co>
2025-05-08 21:49:31 -0500

    Login, registar y activar cuenta funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { AuthenticateComponent } from './auth/authenticate/authenticate.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
+  { path: 'autenticacion', component: AuthenticateComponent },
   { path: '**', redirectTo: 'login' }
 ];
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,24 +1,40 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule,],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+
+  goToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
+  goToAuthenticate() {
+    this.router.navigate(['/autenticacion']); 
+  }
+
   nombre = '';
   email = '';
   password = '';
   telefono = '';
   ciudad = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService, 
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   onRegister() {
     const usuario = {
@@ -32,12 +48,21 @@ export class RegisterComponent {
     this.authService.register(usuario).subscribe({
       next: res => {
         console.log('✅ Registro exitoso:', res);
-        alert('Usuario registrado correctamente');
+  
+        this.snackBar.open('Código Enviado\nIngresa el código de 6 dígitos que has recibido', 'Cerrar', {
+          duration: 5000,
+          panelClass: ['custom-snackbar']
+        });
+  
+        this.router.navigate(['/autenticacion']);
       },
       error: err => {
         console.error('❌ Error en registro:', err);
-        alert('Error al registrar usuario');
+        this.snackBar.open('Error al registrar usuario', 'Cerrar', {
+          duration: 3000
+        });
       }
     });
   }
+  
 }
